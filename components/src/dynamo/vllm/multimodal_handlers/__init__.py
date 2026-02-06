e8a78f21d8f01c4def3bc33d5912e0944abf4fe6
Qi Wang <qiwa@nvidia.com>
2026-02-06 13:28:08 -0800

    refactor: break down PreprocessedHandler

--- components/src/dynamo/vllm/multimodal_handlers/__init__.py
+++ components/src/dynamo/vllm/multimodal_handlers/__init__.py
@@ -8,17 +8,14 @@ from dynamo.vllm.multimodal_handlers.encode_worker_handler import (
 from dynamo.vllm.multimodal_handlers.multimodal_pd_worker_handler import (
     MultimodalPDWorkerHandler,
 )
-from dynamo.vllm.multimodal_handlers.preprocessed_handler import (
-    ECProcessorHandler,
-    PreprocessedHandler,
+from dynamo.vllm.multimodal_handlers.multimodal_pd_worker_handler import (
+    MultimodalPDWorkerHandler,
 )
 from dynamo.vllm.multimodal_handlers.worker_handler import MultimodalDecodeWorkerHandler
 
 __all__ = [
     "EncodeWorkerHandler",
     "VLLMEncodeWorkerHandler",
-    "PreprocessedHandler",
-    "ECProcessorHandler",
     "MultimodalPDWorkerHandler",
     "MultimodalDecodeWorkerHandler",
 ]
